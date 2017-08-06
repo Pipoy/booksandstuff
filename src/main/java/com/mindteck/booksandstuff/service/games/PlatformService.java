cdc410da8dd0c2dc61badbf140e113c80df7b511
Pipoy <philiplozada@live.com>
2017-08-06 15:18:01 -0400

    add platformservice / impl

---
+++ src/main/java/com/mindteck/booksandstuff/service/games/PlatformService.java
@@ -0,0 +1,10 @@
+package com.mindteck.booksandstuff.service.games;
+
+import java.util.Map;
+
+/**
+ * Created by Philip Lozada on 8/6/2017.
+ */
+public interface PlatformService {
+	Map<String, String> getPlatforms();
+}
